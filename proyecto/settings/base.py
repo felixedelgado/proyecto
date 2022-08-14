4bd37205c7a462a50f4b259e236c8e9de0032914
felixedelgado <felixedelgado@gmail.com>
2022-08-14 17:15:30 -0300

    static

--- proyecto/settings/base.py
+++ proyecto/settings/base.py
@@ -123,7 +123,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), 'static'),
-    # os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 MEDIA_URL = '/media/'
